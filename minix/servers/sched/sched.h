e3fbb9f9f7613ebd3a851a19d9988dcf9655d07a
Felipe Soares Calderaro <97483564+calderar0@users.noreply.github.com>
2025-06-27 13:15:50 -0300

    Update sched.h

--- minix/servers/sched/sched.h
+++ minix/servers/sched/sched.h
@@ -3,6 +3,20 @@
  */
 #define _SYSTEM		1	/* tell headers that this is the kernel */
 
+#define BURST_ESTIMATION_ALPHA 50   /* Alpha parameter for exponential averaging (0-100) */
+#define DEFAULT_BURST_ESTIMATE 100  /* Default burst time estimate for new processes */
+#define MIN_BURST_ESTIMATE 10       /* Minimum burst estimate to prevent starvation */
+#define MAX_BURST_ESTIMATE 1000     /* Maximum burst estimate */
+
+/* Se estas constantes não existirem, adicione também: */
+#ifndef USER_Q
+#define USER_Q 12                   /* First user queue */
+#endif
+
+#ifndef MIN_USER_Q  
+#define MIN_USER_Q 14               /* Last user queue */
+#endif
+
 /* The following are so basic, all the *.c files get them automatically. */
 #include <minix/config.h>	/* MUST be first */
 #include <sys/types.h>
